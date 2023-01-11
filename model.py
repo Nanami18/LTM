f558cad72beaacbf9eed2a7f370e7895dff306d4
Thomas <nanami17@cs.washington.edu>
2023-01-10 17:14:22 -0800

    acmodel with visual embedding

--- model.py
+++ model.py
@@ -3,7 +3,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.distributions.categorical import Categorical
 import torch_ac
-
+from minigrid.core.constants import (
+    COLOR_TO_IDX,
+    COLORS,
+    IDX_TO_COLOR,
+    IDX_TO_OBJECT,
+    OBJECT_TO_IDX,
+    DIR_TO_VEC
+)
 
 # Function from https://github.com/ikostrikov/pytorch-a2c-ppo-acktr/blob/master/model.py
 def init_params(m):
@@ -14,6 +21,107 @@ def init_params(m):
         if m.bias is not None:
             m.bias.data.fill_(0)
 
+class ACModelWithEmbed(nn.Module, torch_ac.RecurrentACModel):
+    def __init__(self, obs_space, action_space, use_memory=False, use_text=False, embed_size=16):
+        super().__init__()
+
+        # Decide which components are enabled
+        self.use_text = use_text
+        self.use_memory = use_memory
+       
+        # Define image embedding
+        self.object_embed = nn.Embedding(len(OBJECT_TO_IDX), embed_size)
+        self.color_embed = nn.Embedding(len(COLOR_TO_IDX), embed_size)
+        self.state_embed = nn.Embedding(2, embed_size)
+
+        self.image_conv = nn.Sequential(
+            nn.Conv2d(embed_size*3, 64, (2, 2)),
+            nn.ReLU(),
+            nn.MaxPool2d((2, 2)),
+            nn.Conv2d(64, 64, (2, 2)),
+            nn.ReLU(),
+            nn.Conv2d(64, 128, (2, 2)),
+            nn.ReLU()
+        )
+
+        self.image_embedding_size = 128
+        self.embedding_size = self.image_embedding_size
+
+        # Define memory
+        if self.use_memory:
+            self.memory_rnn = nn.LSTMCell(self.image_embedding_size, self.semi_memory_size)
+
+        # Define text embedding
+        if self.use_text:
+            self.word_embedding_size = 32
+            self.word_embedding = nn.Embedding(obs_space["text"], self.word_embedding_size)
+            self.text_embedding_size = 128
+            self.text_rnn = nn.GRU(self.word_embedding_size, self.text_embedding_size, batch_first=True)
+
+        # Resize image embedding
+        self.embedding_size = self.semi_memory_size
+        if self.use_text:
+            self.embedding_size += self.text_embedding_size
+
+        # Define actor's model
+        self.actor = nn.Sequential(
+            nn.Linear(self.embedding_size, 64),
+            nn.Tanh(),
+            nn.Linear(64, action_space.n)
+        )
+
+        # Define critic's model
+        self.critic = nn.Sequential(
+            nn.Linear(self.embedding_size, 64),
+            nn.Tanh(),
+            nn.Linear(64, 1)
+        )
+
+        # Initialize parameters correctly
+        self.apply(init_params)
+
+    @property
+    def memory_size(self):
+        return 2*self.semi_memory_size
+
+    @property
+    def semi_memory_size(self):
+        return self.image_embedding_size
+
+    def forward(self, obs, memory):
+        
+        x = obs.image
+        object_embedding = self.object_embed(x[:, :, :, 0].long())
+        color_embedding = self.color_embed(x[:, :, :, 1].long())
+        state_embedding = self.state_embed(x[:, :, :, 2].long())
+        im_embed = torch.cat((color_embedding, object_embedding, state_embedding), dim=3)
+        im_embed = im_embed.transpose(1,3).transpose(2,3)
+        x = self.image_conv(im_embed)
+        x = x.reshape(x.shape[0], -1)
+
+        if self.use_memory:
+            hidden = (memory[:, :self.semi_memory_size], memory[:, self.semi_memory_size:])
+            hidden = self.memory_rnn(x, hidden)
+            embedding = hidden[0]
+            memory = torch.cat(hidden, dim=1)
+        else:
+            embedding = x
+
+        if self.use_text:
+            embed_text = self._get_embed_text(obs.text)
+            embedding = torch.cat((embedding, embed_text), dim=1)
+
+        x = self.actor(embedding)
+        dist = Categorical(logits=F.log_softmax(x, dim=1))
+
+        x = self.critic(embedding)
+        value = x.squeeze(1)
+
+        return dist, value, memory
+
+    def _get_embed_text(self, text):
+        _, hidden = self.text_rnn(self.word_embedding(text))
+        return hidden[-1]
 
 class ACModel(nn.Module, torch_ac.RecurrentACModel):
     def __init__(self, obs_space, action_space, use_memory=False, use_text=False):
@@ -80,8 +188,6 @@ class ACModel(nn.Module, torch_ac.RecurrentACModel):
 
     def forward(self, obs, memory):
         x = obs.image.transpose(1, 3).transpose(2, 3)
-        # print(x)
-        # breakpoint()
         x = self.image_conv(x)
         x = x.reshape(x.shape[0], -1)
 
@@ -92,6 +198,7 @@ class ACModel(nn.Module, torch_ac.RecurrentACModel):
             memory = torch.cat(hidden, dim=1)
         else:
             embedding = x
+
         if self.use_text:
             embed_text = self._get_embed_text(obs.text)
             embedding = torch.cat((embedding, embed_text), dim=1)
