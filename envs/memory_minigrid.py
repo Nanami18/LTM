63b8ca67054ae49f46e067a90021bfd12b7aebc5
Thomas <nanami17@cs.washington.edu>
2023-01-29 14:49:05 -0800

    ENV: customized minigrid environments based on 'memory' minigrid

--- envs/memory_minigrid.py
+++ envs/memory_minigrid.py
@@ -22,6 +22,26 @@ from minigrid.envs import MemoryEnv
 
 def register_envs():
     gym.register(
+        id="MiniGrid-MemoryS37-v0-seeobj",
+        entry_point="envs.memory_minigrid:MiniGrid_MemoryS13_v0_seeobj",
+        kwargs={"size":37}
+    )
+    gym.register(
+        id="MiniGrid-MemoryS31-v0-seeobj",
+        entry_point="envs.memory_minigrid:MiniGrid_MemoryS13_v0_seeobj",
+        kwargs={"size":31}
+    )
+    gym.register(
+        id="MiniGrid-MemoryS25-v0-seeobj",
+        entry_point="envs.memory_minigrid:MiniGrid_MemoryS13_v0_seeobj",
+        kwargs={"size":25}
+    )
+    gym.register(
+        id="MiniGrid-MemoryS19-v0-seeobj",
+        entry_point="envs.memory_minigrid:MiniGrid_MemoryS13_v0_seeobj",
+        kwargs={"size":19}
+    )
+    gym.register(
         id="MiniGrid-MemoryS13-v0-seeobj",
         entry_point="envs.memory_minigrid:MiniGrid_MemoryS13_v0_seeobj",
     )
@@ -30,8 +50,116 @@ def register_envs():
         entry_point="envs.memory_minigrid:MiniGrid_MemoryS13_v0_seeobj",
         kwargs={"size":9}
     )
+    gym.register(
+        id="MiniGrid-MemoryS5-v0-seeobj",
+        entry_point="envs.memory_minigrid:MiniGrid_MemoryS13_v0_seeobj",
+        kwargs={"size":5}
+    )
+    gym.register(
+        id="MiniGrid-MemoryS5-v0-seeobj-myopic",
+        entry_point="envs.memory_minigrid:MiniGrid_MemoryS13_v0_seeobj",
+        kwargs={"size":5, "agent_view_size":3, "max_steps_coeff":1}
+    )
+    gym.register(
+        id="MiniGrid-MemoryS7-v0-seeobj-myopic",
+        entry_point="envs.memory_minigrid:MiniGrid_MemoryS13_v0_seeobj",
+        kwargs={"size":7, "agent_view_size":5, "max_steps_coeff":2}
+    )
+    gym.register(
+        id="MiniGrid-MemoryS9-v0-hallwayobj",
+        entry_point="envs.memory_minigrid:MiniGrid_MemoryS13_v0_hallwayobj",
+        kwargs={"size":9}
+    )
 
 class MiniGrid_MemoryS13_v0_seeobj(MemoryEnv):
+    """
+    This environment is a memory test. The agent starts in a small room
+    where it sees an object. It then has to go through a narrow hallway
+    which ends in a split. At each end of the split there is an object,
+    one of which is the same as the object in the starting room. The
+    agent has to remember the initial object, and go to the matching
+    object at split.
+    """
+
+    def __init__(
+        self,
+        size=13,
+        random_length=False,
+        max_steps_coeff = 5,
+        **kwargs,
+    ):
+        self.random_length = random_length
+        super().__init__(
+            size=size,
+            max_steps=max_steps_coeff*size**2,
+            # Set this to True for maximum speed
+            # see_through_walls=False, # The MemoryEnv doesn't take this argument, and always pass false to super(), so we can't set it here
+            **kwargs,
+        )
+
+    def _gen_grid(self, width, height):
+        self.grid = Grid(width, height)
+
+        # Generate the surrounding walls
+        self.grid.horz_wall(0, 0)
+        self.grid.horz_wall(0, height-1)
+        self.grid.vert_wall(0, 0)
+        self.grid.vert_wall(width - 1, 0)
+
+        assert height % 2 == 1
+        upper_room_wall = height // 2 - 2
+        lower_room_wall = height // 2 + 2
+        if self.random_length:
+            hallway_end = self._rand_int(4, width - 2)
+        else:
+            hallway_end = width - 3
+
+        # Start room
+        for i in range(1, 5):
+            self.grid.set(i, upper_room_wall, Wall())
+            self.grid.set(i, lower_room_wall, Wall())
+        self.grid.set(4, upper_room_wall + 1, Wall())
+        self.grid.set(4, lower_room_wall - 1, Wall())
+
+        # Horizontal hallway
+        for i in range(5, hallway_end):
+            self.grid.set(i, upper_room_wall + 1, Wall())
+            self.grid.set(i, lower_room_wall - 1, Wall())
+
+        # Vertical hallway
+        for j in range(0, height):
+            if j != height // 2:
+                self.grid.set(hallway_end, j, Wall())
+            self.grid.set(hallway_end + 2, j, Wall())
+
+        # Fix the player's start position and orientation
+        self.agent_pos = (1, height // 2)
+        self.agent_dir = 0
+
+        # Place objects
+        start_room_obj = self._rand_elem([Key, Ball])
+        self.grid.set(1, height // 2 - 1, start_room_obj('green'))
+
+        other_objs = self._rand_elem([[Ball, Key], [Key, Ball]])
+        pos0 = (hallway_end + 1, height // 2 - 2)
+        pos1 = (hallway_end + 1, height // 2 + 2)
+        self.grid.set(*pos0, other_objs[0]('green'))
+        self.grid.set(*pos1, other_objs[1]('green'))
+
+        # Choose the target objects
+        if start_room_obj == other_objs[0]:
+            self.success_pos = (pos0[0], pos0[1] + 1)
+            self.failure_pos = (pos1[0], pos1[1] - 1)
+        else:
+            self.success_pos = (pos1[0], pos1[1] - 1)
+            self.failure_pos = (pos0[0], pos0[1] + 1)
+        
+        # self.place_agent(top=(2,4), size=(1,1))
+
+        self.mission = 'go to the matching object at the end of the hallway'
+
+
+class MiniGrid_MemoryS13_v0_hallwayobj(MemoryEnv):
     """
     This environment is a memory test. The agent starts in a small room
     where it sees an object. It then has to go through a narrow hallway
@@ -98,8 +226,20 @@ class MiniGrid_MemoryS13_v0_seeobj(MemoryEnv):
         # Place objects
         start_room_obj = self._rand_elem([Key, Ball])
         self.grid.set(1, height // 2 - 1, start_room_obj('green'))
+        self.grid.set(2, height // 2 - 1, start_room_obj('green'))
+        self.grid.set(3, height // 2 - 1, start_room_obj('green'))
+        self.grid.set(4, height // 2 - 1, start_room_obj('green'))
+        self.grid.set(5, height // 2 - 1, start_room_obj('green'))
+        self.grid.set(6, height // 2 - 1, start_room_obj('green'))
 
-        other_objs = self._rand_elem([[Ball, Key], [Key, Ball]])
+        self.grid.set(1, height // 2 + 1, start_room_obj('green'))
+        self.grid.set(2, height // 2 + 1, start_room_obj('green'))
+        self.grid.set(3, height // 2 + 1, start_room_obj('green'))
+        self.grid.set(4, height // 2 + 1, start_room_obj('green'))
+        self.grid.set(5, height // 2 + 1, start_room_obj('green'))
+        self.grid.set(6, height // 2 + 1, start_room_obj('green'))
+
+        other_objs = self._rand_elem([[Ball, Key]])
         pos0 = (hallway_end + 1, height // 2 - 2)
         pos1 = (hallway_end + 1, height // 2 + 2)
         self.grid.set(*pos0, other_objs[0]('green'))
