1cb8cd8f5da04b24efb99cb950c3af967e77f606
Thomas <nanami17@cs.washington.edu>
2023-05-22 10:36:25 -0700

    basic dt training/evaluation pipeline

---
+++ decision_transformer/dataset_generation.py
@@ -0,0 +1,131 @@
+import numpy as np
+import gymnasium as gym
+import torch.nn as nn
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+# Generate a dataset of expert demonstrations, stored as an tuple of (s,a,r) arrays
+def generate_expert_trajectories(env, episodes, reward_pertubation=0.0):
+    
+    data = []
+    for i in range(episodes):
+        cur_obs = []
+        cur_acts = []
+        cur_rewards = []
+        terminated = False
+        truncated = False
+
+        obs, _ = env.reset()
+        cur_obs.append(obs)
+        while not terminated and not truncated:
+            action = env.compute_expert_action()
+            cur_acts.append(action)
+            obs, reward, terminated, truncated, _ = env.step(action)
+            cur_rewards.append(reward)
+            if not terminated:
+                cur_obs.append(obs)
+        
+        data.append((cur_obs, cur_acts, cur_rewards))
+
+    return data
+
+def generate_random_trajectories(env, episodes):
+    
+    data = []
+    for i in range(episodes):
+        cur_obs = []
+        cur_acts = []
+        cur_rewards = []
+        terminated = False
+        truncated = False
+
+        obs, _ = env.reset()
+        cur_obs.append(obs)
+        while not terminated and not truncated:
+            action = env.compute_random_action()
+            cur_acts.append(action)
+            obs, reward, terminated, truncated, _ = env.step(action)
+            cur_rewards.append(reward)
+            if not terminated:
+                cur_obs.append(obs)
+        
+        data.append((cur_obs, cur_acts, cur_rewards))
+
+    return data
+
+class HallwayMemoryEnvDataset(Dataset):
+    # Trajectories is supposed to be list of episodes, each element is a tuple of (s,a,r) arrays
+    def __init__(self, trajectories, context_lenth, gamma):
+        self.trajectories = trajectories
+        self.num_traj = len(trajectories)
+        self.context_length = context_lenth
+        self.gamma = gamma
+        self.image_obs_dim = trajectories[0][0][0]['image'].shape
+        self.act_dim = 1
+    
+    def __len__(self):
+        return len(self.trajectories)
+    
+    def _discount_cumsum(self, x, gamma):
+        discount_cumsum = np.zeros_like(x)
+        discount_cumsum[-1] = x[-1]
+        for t in reversed(range(len(x)-1)):
+            discount_cumsum[t] = x[t] + gamma * discount_cumsum[t+1]
+        
+        return discount_cumsum
+    
+    def __getitem__(self, idx):
+        cur_traj = self.trajectories[idx]
+        start = np.random.randint(0, len(cur_traj[0])-1)
+        end = min(start + self.context_length, len(cur_traj[0]))
+        states = [obs['image'] for obs in cur_traj[0][start:end]]
+        actions = cur_traj[1][start:end]
+        rewards = cur_traj[2][start:end]
+        rewards = self._discount_cumsum(rewards, self.gamma)
+        if self.act_dim == 1:
+            actions = np.expand_dims(actions, axis=-1)
+        rewards = np.expand_dims(rewards, axis=-1)
+
+        # Pad to right
+        # states = np.concatenate([states, np.zeros((self.context_length - len(states), *self.image_obs_dim))], axis=0)
+        # actions = np.concatenate([actions, np.zeros((self.context_length - len(actions), self.act_dim))], axis=0)
+        # rewards = np.concatenate([rewards, np.zeros((self.context_length - len(rewards)))], axis=0)
+        # timesteps = np.concatenate((np.arange(start, end), np.zeros(self.context_length - (end-start))), axis=0)
+        
+        # mask == 1 `states` is valid, mask == 0 `states` is invalid (padding)
+        # mask = np.concatenate((np.ones(end-start), np.zeros(self.context_length - (end-start))), axis=0)
+
+        # Pad to left
+        states = np.concatenate([np.zeros((self.context_length - len(states), *self.image_obs_dim)), states], axis=0)
+        actions = np.concatenate([np.zeros((self.context_length - len(actions), self.act_dim)), actions], axis=0)
+        rewards = np.concatenate([np.zeros((self.context_length - len(rewards), 1)), rewards], axis=0)
+        timesteps = np.concatenate((np.zeros(self.context_length - (end-start)), np.arange(start, end)), axis=0)
+
+        # mask == 1 `states` is valid, mask == 0 `states` is invalid (padding)
+        mask = np.concatenate((np.zeros(self.context_length - (end-start)), np.ones(end-start)), axis=0)
+
+        # Convert to torch tensors
+        states = torch.from_numpy(states).float()
+        actions = torch.from_numpy(actions).long()
+        rewards = torch.from_numpy(rewards).float()
+        timesteps = torch.from_numpy(timesteps).long()
+
+        return states, actions, rewards, timesteps, mask
+
+def collate_fn(batch):
+    pass
+
+
+if __name__ == "__main__":
+    import argparse
+    import envs
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--env", type=str, required=True)
+    parser.add_argument("--episodes", type=int, default=10, required=True)
+
+    args = parser.parse_args()
+    
+    env = gym.make(args.env)
+    trajectories = generate_expert_trajectories(env, args.episodes)
+    data = HallwayMemoryEnvDataset(trajectories, 10, 0.99)
---
+++ decision_transformer/train.py
@@ -0,0 +1,141 @@
+import argparse
+import time
+import datetime
+import torch_ac
+import torch
+from torch.utils.data import DataLoader
+import numpy as np
+import tensorboardX
+import sys
+
+import utils
+from utils import device
+from configs.config import cfg, cfg_from_file
+
+import envs
+from models.decision_transformer_model import build_model
+from decision_transformer.dataset_generation import generate_expert_trajectories, generate_random_trajectories, HallwayMemoryEnvDataset
+
+
+# Parse arguments
+
+parser = argparse.ArgumentParser()
+
+# General parameters
+parser.add_argument("--config", type=str, required=True)
+parser.add_argument("--custom_dir", type=str, default=None)
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    cfg_from_file(args.config)
+    
+    cfg.mem = cfg.recurrence > 1
+
+    # Set run dir
+
+    date = datetime.datetime.now().strftime("%y-%m-%d-%H-%M-%S")
+    default_model_name = f"{cfg.env_name}_{cfg.algo}_seed{cfg.seed}_{date}"
+    
+    if args.custom_dir is not None:
+        model_name = args.custom_dir
+    else:
+        model_name = str(args.config).split("/")[-1][:-5] or default_model_name
+    model_dir = utils.get_model_dir(model_name)
+    # Load loggers and Tensorboard writer
+
+    txt_logger = utils.get_txt_logger(model_dir)
+    csv_file, csv_logger = utils.get_csv_logger(model_dir)
+    tb_writer = tensorboardX.SummaryWriter(model_dir)
+
+    # Log command and all script arguments
+    txt_logger.info("{}\n".format(" ".join(sys.argv)))
+    txt_logger.info("{}\n".format(cfg))
+
+    # Set seed for all randomness sources
+
+    utils.seed(cfg.seed)
+
+    # Set device
+
+    txt_logger.info(f"Device: {device}\n")
+
+    # Load training status
+
+    try:
+        status = utils.get_status(model_dir)
+    except OSError:
+        status = {"num_frames": 0, "update": 0}
+    txt_logger.info("Training status loaded\n")
+
+    # Create env
+    env = utils.make_env(cfg.env_name, cfg.seed)
+
+    # Load model
+    model = build_model(cfg, env.observation_space, env.action_space)
+    model.to(device)
+    if "model_state" in status:
+        model.load_state_dict(status["model_state"])
+    txt_logger.info("Model loaded\n")
+    txt_logger.info("{}\n".format(model))
+
+    # Load optimizer
+    optimizer = torch.optim.AdamW(model.parameters(), lr=cfg.lr, eps=cfg.optim_eps)
+    if "optimizer_state" in status:
+        optimizer.load_state_dict(status["optimizer_state"])
+    txt_logger.info("Optimizer loaded\n")
+
+    # Load expert dataset
+    trajectories = generate_expert_trajectories(env, cfg.num_episodes, cfg.reward_pertubation)
+    print("Estimated epochs: {}".format(cfg.frames/cfg.num_episodes))
+    train_ds = HallwayMemoryEnvDataset(trajectories, cfg.context_length, cfg.discount)
+
+    # Build dataloader
+    train_loader = DataLoader(train_ds, batch_size=cfg.batch_size, shuffle=True, num_workers=cfg.num_workers)
+
+    # Train model
+    model.train()
+    trained_frames = 0
+    epoch_count = 0
+    start_time = time.time()
+
+    while trained_frames < cfg.frames:
+        cum_loss = 0
+        for batch in train_loader:
+            states, actions, rewards, timesteps, masks = batch
+            states = states.to(device)
+            actions = actions.to(device)
+            rewards = rewards.to(device)
+            timesteps = timesteps.to(device)
+            masks = masks.to(device)
+
+            # breakpoint()
+            action_logits = model(rewards, states, actions, timesteps)
+            
+            # Compute loss and update the model
+            if cfg.discrete_action:
+                loss = torch.nn.functional.cross_entropy(action_logits.permute(0, 2, 1), actions.squeeze(2))
+            else:
+                loss = torch.nn.functional.mse_loss(action_logits, actions)
+
+            optimizer.zero_grad()
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), cfg.max_grad_norm)
+            optimizer.step()
+            cum_loss += loss.item()
+
+            trained_frames += states.shape[0]
+            
+        epoch_count += 1
+        if epoch_count % cfg.log_interval == 0:
+            training_time = time.time() - start_time
+            txt_logger.info(f"Epoch {epoch_count} | Loss: {cum_loss} | Training time: {training_time}")
+
+        if epoch_count % cfg.save_interval == 0:
+            status = {"num_frames": trained_frames, "model_state": model.state_dict(), "optimizer_state": optimizer.state_dict(), "update": epoch_count}
+            utils.save_status(status, model_dir)
+            txt_logger.info("Status saved")
+
+    status = {"num_frames": trained_frames, "model_state": model.state_dict(), "optimizer_state": optimizer.state_dict(), "update": epoch_count}
+    utils.save_status(status, model_dir)
+    txt_logger.info("Training Completed, saved final checkpoint")
+
