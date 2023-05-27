a4760927019019ef7234aff3288c1655c1dfa952
Thomas <nanami17@cs.washington.edu>
2023-05-27 14:12:25 -0700

    concatenate target embedding afterward

--- models/lstm_model.py
+++ models/lstm_model.py
@@ -13,11 +13,14 @@ from minigrid.core.constants import (
 )
 
 def build_model(cfg, obs_space, action_space):
-    if cfg.use_embed:
-        if cfg.use_linear_procs:
-            model = ACModelWithLinearProcs(obs_space, action_space, cfg)
-        else:
-             model = ACModelWithEmbed(obs_space, action_space,cfg)
+    if "ObjLocate" in cfg.env_name:
+        model = ACModelWithEmbed_findobj(obs_space, action_space, cfg)
+    elif "memory" in cfg.env_name:
+        if cfg.use_embed:
+            if cfg.use_linear_procs:
+                model = ACModelWithLinearProcs(obs_space, action_space, cfg)
+            else:
+                model = ACModelWithEmbed(obs_space, action_space,cfg)
     else:
         model = ACModel(obs_space, action_space, cfg)
     
@@ -207,6 +210,112 @@ class ACModelWithEmbed(nn.Module, torch_ac.RecurrentACModel):
         if self.use_text:
             embed_text = self._get_embed_text(obs.text)
             embedding = torch.cat((embedding, embed_text), dim=1)
+
+        x = self.actor(embedding)
+        dist = Categorical(logits=F.log_softmax(x, dim=1))
+        x = self.critic(embedding)
+        value = x.squeeze(1)
+        return dist, value, memory
+
+    def _get_embed_text(self, text):
+        _, hidden = self.text_rnn(self.word_embedding(text))
+        return hidden[-1]
+
+class ACModelWithEmbed_findobj(nn.Module, torch_ac.RecurrentACModel):
+    def __init__(self, obs_space, action_space, cfg):
+        super().__init__()
+
+        # Decide which components are enabled
+        self.use_text = cfg.use_text
+        self.use_memory = cfg.use_memory
+       
+        # Define image embedding
+        self.object_embed = nn.Embedding(len(OBJECT_TO_IDX), cfg.token_embed_size)
+        self.color_embed = nn.Embedding(len(COLOR_TO_IDX), cfg.token_embed_size)
+        self.state_embed = nn.Embedding(2, cfg.token_embed_size)
+
+        self.image_conv = nn.Sequential(
+            nn.Conv2d(cfg.token_embed_size*3, cfg.image_embed_size//2, (2, 2)),
+            nn.ReLU(),
+            nn.MaxPool2d((2, 2)),
+            nn.Conv2d(cfg.image_embed_size//2, cfg.image_embed_size//2, (2, 2)),
+            nn.ReLU(),
+            nn.Conv2d(cfg.image_embed_size//2, cfg.image_embed_size, (2, 2)),
+            nn.ReLU()
+        )
+
+        self.image_embedding_size = cfg.image_embed_size
+        self.token_embed_size = cfg.token_embed_size
+
+        # Define memory
+        if self.use_memory:
+            self.memory_rnn = nn.LSTMCell(self.image_embedding_size, self.semi_memory_size)
+
+        # Define text embedding
+        if self.use_text:
+            self.word_embedding_size = 32
+            self.word_embedding = nn.Embedding(obs_space["text"], self.word_embedding_size)
+            self.text_embedding_size = 128
+            self.text_rnn = nn.GRU(self.word_embedding_size, self.text_embedding_size, batch_first=True)
+
+        # Resize image embedding
+        self.embedding_size = self.semi_memory_size + self.token_embed_size
+        if self.use_text:
+            self.embedding_size += self.text_embedding_size
+
+        # Define actor's model
+        self.actor = nn.Sequential(
+            nn.Linear(self.embedding_size, self.embedding_size),
+            nn.Tanh(),
+            nn.Linear(self.embedding_size, action_space.n)
+        )
+
+        # Define critic's model
+        self.critic = nn.Sequential(
+            nn.Linear(self.embedding_size, self.embedding_size),
+            nn.Tanh(),
+            nn.Linear(self.embedding_size, 1)
+        )
+
+        # Initialize parameters correctly
+        init_params(self, cfg)
+
+    @property
+    def memory_size(self):
+        return 2*self.semi_memory_size
+
+    @property
+    def semi_memory_size(self):
+        return self.image_embedding_size
+
+    def forward(self, obs, memory):
+        
+        x = obs.image
+        target_color = obs.target_color
+
+        object_embedding = self.object_embed(x[:, :, :, 0].long())
+        color_embedding = self.color_embed(x[:, :, :, 1].long())
+        state_embedding = self.state_embed(x[:, :, :, 2].long())
+
+        im_embed = torch.cat((color_embedding, object_embedding, state_embedding), dim=3)
+        im_embed = im_embed.transpose(1,3).transpose(2,3)
+        x = self.image_conv(im_embed)
+        x = x.reshape(x.shape[0], -1)
+
+        if self.use_memory:
+            hidden = (memory[:, :self.semi_memory_size], memory[:, self.semi_memory_size:])
+            hidden = self.memory_rnn(x, hidden)
+            embedding = hidden[0]
+            memory = torch.cat(hidden, dim=1)
+        else:
+            embedding = x
+
+        if self.use_text:
+            embed_text = self._get_embed_text(obs.text)
+            embedding = torch.cat((embedding, embed_text), dim=1)
+        
+        target_embedding = self.color_embed(target_color.long())
+        embedding = torch.cat((embedding, target_embedding), dim=1)
 
         x = self.actor(embedding)
         dist = Categorical(logits=F.log_softmax(x, dim=1))
